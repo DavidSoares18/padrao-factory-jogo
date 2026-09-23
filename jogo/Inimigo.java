0043eb38f5839867591bb55859aa76bea97b09b8
David Soares <137106233+Soa1804@users.noreply.github.com>
2026-09-23 20:51:39 -0300

    Cria as classes base Inimigo e Localizacao para o padrao Factory Method

--- jogo/Inimigo.java
+++ jogo/Inimigo.java
@@ -1,5 +1,9 @@
 package jogo;
 
-public class Inimigo {
-    
+/**
+ * Produto do Factory Method: todo inimigo, de qualquer localizacao,
+ * sabe atacar o jogador.
+ */
+public interface Inimigo {
+    void atacar(Jogador jogador);
 }
--- jogo/Localizacao.java
+++ jogo/Localizacao.java
@@ -1,5 +1,23 @@
 package jogo;
 
-public class Localizacao {
-    
+/**
+ * Criador do Factory Method.
+ * Define o que acontece quando o jogador chega (receberJogador),
+ * mas deixa para as subclasses decidir QUAL inimigo criar (criarInimigo).
+ */
+public abstract class Localizacao {
+    private final String nome;
+
+    protected Localizacao(String nome) {
+        this.nome = nome;
+    }
+
+    // Factory method: cada localizacao concreta decide o tipo de inimigo
+    protected abstract Inimigo criarInimigo();
+
+    public void receberJogador(Jogador jogador) {
+        System.out.println(jogador.getNome() + " entrou na fase: " + nome);
+        Inimigo inimigo = criarInimigo(); // nao sabe (nem precisa saber) a classe concreta
+        inimigo.atacar(jogador);          // garantia: sempre ha um inimigo para enfrentar
+    }
 }
